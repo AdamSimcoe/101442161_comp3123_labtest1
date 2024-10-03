7d75a9447c1abc257c68409ec8291140624d11c8
Adam Simcoe <101442161@georgebrown.ca>
2024-10-03 12:48:30 -0400

    Completed question 1, added question 2 file

--- question-1/question1.js
+++ question-1/question1.js
@@ -1,5 +1,27 @@
 // Created by Adam Simcoe - 101442161
 // Last Updated on October 3rd 2024
 
+/* Question 1 - Create script with a function named lowerCaseWords that takes a mixed array as input, 
+   returns a promise that is resolved or rejected, and filters all non-strings before lower casing 
+   the remaining strings */
+
 const mixedArray = ['PIZZA', 10, true, 25, false, 'Wings'];
 
+function lowerCaseWords (array) {
+    const filteredArray = array.filter(arrayItem => typeof arrayItem === 'string');
+    const lowerCaseStrings = filteredArray.map(string => string.toLowerCase());
+
+    return new Promise((resolve, reject) => {
+
+        if (!Array.isArray(array)) {
+            reject('This is not an array. Please try again with a valid array.');
+        }
+        else {
+            resolve(lowerCaseStrings);
+        }
+    });
+}
+
+lowerCaseWords(mixedArray)
+    .then(output => console.log(output))
+    .catch(err => console.error(err));
---
+++ question-2/question2.js
@@ -0,0 +1,9 @@
+// Created by Adam Simcoe
+// Last Updated October 3rd 2024
+
+/* Question 2 - Create a method resolvedPromise that is similar to delayedSuccess and 
+   resolves a message after a timeout of 500ms. Create a method rejectedPromise that is 
+   similar to delayedException and rejects an error message after a timeout of 500ms. 
+   Call both promises separately and handle the resolves and reject results and then output to the console. */
+
+ 
