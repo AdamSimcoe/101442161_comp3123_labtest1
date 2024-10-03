e4e637abb145ac90a4b2f2a587881d1f71f71b74
Adam Simcoe <101442161@georgebrown.ca>
2024-10-03 13:29:51 -0400

    Completed question 2

--- question-2/question2.js
+++ question-2/question2.js
@@ -6,4 +6,31 @@
    similar to delayedException and rejects an error message after a timeout of 500ms. 
    Call both promises separately and handle the resolves and reject results and then output to the console. */
 
- 
+ class PromiseHandlers {
+
+    resolvedPromise() {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve({'message': 'Resolved promise in 500ms.'})
+            }, 500);
+        });
+    }
+
+    rejectedPromise() {
+        return new Promise((reject) => {
+            setTimeout(() => {
+                reject({ error: 'Rejected promise in 500ms.'});
+            }, 500);
+        }); 
+    }
+ }
+
+ const handler = new PromiseHandlers();
+
+ handler.resolvedPromise()
+    .then(output => console.log(output))
+    .catch(err => console.error(err));
+
+ handler.rejectedPromise()
+    .then(output => console.log(output))
+    .catch(err => console.error(err));
